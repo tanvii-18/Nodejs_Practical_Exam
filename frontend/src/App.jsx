cd8a757cd80fc591ec516ec5ad1165e1d9b83f54
tanvii-18 <8438tanvipatel@gmail.com>
2026-02-18 11:19:43 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Routes } from "react-router";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
